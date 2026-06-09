77babb4d0478491c98773345f9f24f7c0b646b08
Wilson Furtado <furtadorodrigo88@gmail.com>
2026-06-09 11:26:24 -0100

    localhost modifei

--- _trash/registro/page.tsx
+++
@@ -1,1 +0,0 @@
-export { default } from "../../servidor-local-front/app/(unauth)/register/page";
--- servidor-local/src/docs/swagger.ts
+++ servidor-local/src/docs/swagger.ts
@@ -11,7 +11,7 @@ const options: swaggerJsdoc.Options = {
         },
         servers: [
             {
-                url: `http://localhost:8080`,
+                url: `${process.env.NEXT_PUBLIC_API_URL}`,
                 description: `dev`,
             }
         ],
