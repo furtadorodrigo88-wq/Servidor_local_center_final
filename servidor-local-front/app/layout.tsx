ff41f5e8bdaac3c2882fafbe05984b77c870b033
Wilson Furtado <furtadorodrigo88@gmail.com>
2026-06-09 10:58:53 -0100

    apollo provider

--- servidor-local-front/app/layout.tsx
+++ servidor-local-front/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import { ApolloClientProviders } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,8 +31,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <ApolloClientProviders>
         <Toaster position="top-right" richColors expand />
         {children}
+        </ApolloClientProviders>
       </body>
     </html>
   );
