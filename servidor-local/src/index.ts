020f81dab982f19e573699b64066300b7fc3250c
Wilson Furtado <furtadorodrigo88@gmail.com>
2026-06-11 10:50:36 -0100

    permicoes

--- servidor-local/src/index.ts
+++ servidor-local/src/index.ts
@@ -23,7 +23,7 @@ app.use(express.json()); // para interpretar o corpo das requisições como JSON
 
 // liberta o front-end de aceder ao back-end
 app.use(cors({
-    origin: ["http://localhost:3000", "https://servidor-local-center-backend2.onrender.com", "https://servidor-local-center-final.vercel.app"],
+    origin: ["http://localhost:3000", "https://servidor-local-center-backend-w1rr.onrender.com", "https://servidor-local-center-final.vercel.app", "https://dev-exame-final.vercel.app", "https://servidor-local-center-final.onrender.com"],
     credentials: true,
     allowedHeaders: ["*"],
 }));
