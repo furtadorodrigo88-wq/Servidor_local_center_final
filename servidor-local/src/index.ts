07c62541b946d4abb675140e626dc032d3f82c2f
Wilson Furtado <furtadorodrigo88@gmail.com>
2026-06-22 11:36:19 -0100

    feat: initialize Express and Apollo server with routes, Swagger documentation, and database setup

--- servidor-local/src/index.ts
+++ servidor-local/src/index.ts
@@ -17,8 +17,7 @@ import { ApolloServer } from "@apollo/server";
 import { resolvers, typeDefs } from "./graphql/index.js";
 import { expressMiddleware } from "@as-integrations/express5";
 import { initDatabase } from "./lib/init-db.js";
-import morgan from "morgan"
-import statusMonitor from "express-status-monitor"
+
 
 
 const app = express();
@@ -33,8 +32,6 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 }));
 
-app.use(morgan("dev"));
-app.use(statusMonitor())
 
 // rota inicial do express
 app.get("/", (req: Request, res: Response) => {
